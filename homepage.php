1f998e240dbe3d625ba487f53a44680148b0b5c7
Josh Shannon <89867040+joshs923@users.noreply.github.com>
2026-04-30 16:42:34 -0500

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -2,22 +2,40 @@
 <html>
 <head>
     <title>PharmaTrack</title>
+    <link rel="stylesheet" href="ui_format.css">
 </head>
 <body>
 
+<div class="container">
+
 <h1>PharmaTrack</h1>
+<p>Your one stop shop for all pharmacy operations.</p>
+
+<p>PharmaTrack allows you to manage prescriptions, inventory, patients and supplier records.</p>
+
+<h2>Menu</h2>
 
 <ul>
     <li><a href="prescription_entry.php">Prescription Entry</a></li>
+
     <li><a href="inventory_dashboard.php">Inventory Dashboard</a></li>
+
     <li><a href="reorder_addmeds.php">Reorder/Add Medicine</a></li>
+
     <li><a href="patient_lookup.php">Patient Lookup</a></li>
+
     <li><a href="query_results.php">Query Results</a></li>
+
     <li><a href="update_prescription.php">Update Prescription</a></li>
+
     <li><a href="delete_prescription.php">Delete Prescription</a></li>
-    <li><a href="sql_injection_vulnerable.php">SQL Injection Demo VULNERABLE</a></li>
-    <li><a href="sql_injection_secure.php">SQL Injection Demo SECURE</a></li>
+
+    <li><a href="sql_vulner.php">Vulnerable SQL Injection Demo</a></li>
+    
+    <li><a href="sql_secure.php">Secure SQL Injection Demo</a></li>
 </ul>
+
+</div>
 
 </body>
 </html>
