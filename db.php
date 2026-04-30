9629baa6d0a4cc4ccd776c1f2ddcf15a1640fb72
Josh Shannon <89867040+joshs923@users.noreply.github.com>
2026-04-30 16:42:01 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,7 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "pharmatrack");
 
+// Connection failed. Output error message to user.
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
