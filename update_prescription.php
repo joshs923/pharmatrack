60b785efd5a5455b2281e0f95755fc0d1a581091
Josh Shannon <89867040+joshs923@users.noreply.github.com>
2026-04-30 16:45:38 -0500

    Update update_prescription.php

--- update_prescription.php
+++ update_prescription.php
@@ -10,15 +10,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($conn->query($sql)) {
         echo "Updated successfully";
     } else {
+        // Failed update for some reason. Output error
         echo "Error: " . $conn->error;
     }
 }
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Update Prescription</title>
+    <link rel="stylesheet" href="ui_format.css">
+</head>
+<body>
+
+<div class="container">
+
 <p><a href="homepage.php">Back to Homepage</a></p>
+
+<h2>Update Prescription</h2>
 
 <form method="post">
     Prescription ID: <input type="number" name="presc_id"><br>
     New Status: <input type="text" name="status"><br>
     <input type="submit" value="Update">
 </form>
+
+</div>
+
+</body>
+</html>
